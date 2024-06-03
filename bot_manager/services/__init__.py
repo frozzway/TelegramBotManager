3f9f5a56e692edd881220cfa085a8f781331e253
Александр Окунев <a.okunev@mininform74.ru>
2024-06-03 14:09:56 +0500

    Объединил сервисы элементов в один файл

--- bot_manager/services/__init__.py
+++ bot_manager/services/__init__.py
@@ -2,11 +2,13 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from bot_manager.services.elements_services import LinkButtonService, CategoryService, ScenarioService
 from bot_manager.services.auth_service import AuthService, CurrentUserDp
-from bot_manager.services.category_service import CategoryService
 from bot_manager.services.user_service import UserService
 
 
 AuthServiceDp = Annotated[AuthService, Depends(AuthService)]
 UserServiceDp = Annotated[UserService, Depends(UserService)]
 CategoryServiceDp = Annotated[CategoryService, Depends(CategoryService)]
+LinkButtonServiceDp = Annotated[LinkButtonService, Depends(LinkButtonService)]
+ScenarioServiceDp = Annotated[ScenarioService, Depends(ScenarioService)]
--- bot_manager/services/elements_services.py
+++ bot_manager/services/elements_services.py
@@ -3,7 +3,16 @@ from bot_manager.services.db_dependency import BotSession
 from bot_manager import tables
 
 
+class LinkButtonService(CrudService):
+    def __init__(self, session: BotSession):
+        super().__init__(session, tables.LinkButton)
+
+
 class CategoryService(CrudService):
     def __init__(self, session: BotSession):
         super().__init__(session, tables.Category)
 
+
+class ScenarioService(CrudService):
+    def __init__(self, session: BotSession):
+        super().__init__(session, tables.ScenarioButton)
