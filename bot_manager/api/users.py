39fae5ddce18e357a1d5cbe7f367d5e7b7ed4966
Александр Окунев <a.okunev@mininform74.ru>
2024-06-03 13:11:12 +0500

    Добавил метод set_bot для пользователя

--- bot_manager/api/users.py
+++ bot_manager/api/users.py
@@ -1,9 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
+
+from bot_manager.services import UserServiceDp, CurrentUserDp
+
+router = APIRouter(prefix='/users', tags=['Users'])
 
 
-router = APIRouter()
-
-
-@router.get("/")
-async def get_user():
-    pass
+@router.post("/bot", status_code=status.HTTP_204_NO_CONTENT)
+async def set_bot(
+    bot_id: int,
+    response: Response,
+    user_service: UserServiceDp,
+    current_user: CurrentUserDp
+):
+    if user_service.has_bot_access(bot_id, current_user):
+        response.set_cookie('bot_id', str(bot_id))
