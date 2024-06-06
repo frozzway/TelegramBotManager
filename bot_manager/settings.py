8fef1c8a9b6d032db3963429e8950917a7aaf498
Александр Окунев <a.okunev@mininform74.ru>
2024-06-06 10:46:40 +0500

    Внес настройки для mail_server

--- bot_manager/settings.py
+++ bot_manager/settings.py
@@ -2,6 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    project_name: str = 'BotManager'
+
     server_host: str = '0.0.0.0'
     server_port: int = '8080'
     timezone: str = 'Asia/Yekaterinburg'
@@ -19,6 +21,12 @@ class Settings(BaseSettings):
     jwt_cookie_name: str = 'BotController'
     jwt_secret: str = 'a99ef8a3a0734e2d820dc323a29b787235ab7ec504a870ca0ff8c9df5f058042'
     jwt_algorithm: str = 'HS256'
+
+    hostname: str = 'example.com'
+    email_server: str = f'mail.{hostname}'
+    email_sender: str = f'no-reply@{hostname}'
+    email_account: str = f'no-reply@{hostname}'
+    email_password: str = 'JMBNsbvuhknasdf7124'
 
 
 settings = Settings(
