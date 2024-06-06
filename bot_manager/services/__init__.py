29a84a9ede7517551a3ee5fc94aee0819ab54be5
Александр Окунев <a.okunev@mininform74.ru>
2024-06-06 10:48:01 +0500

    Добавил EmailService в пакет

--- bot_manager/services/__init__.py
+++ bot_manager/services/__init__.py
@@ -2,11 +2,11 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from bot_manager.services.email_service import EmailService
 from bot_manager.services.elements_services import LinkButtonService, CategoryService, ScenarioService
 from bot_manager.services.ownership_service import OwnershipService
 from bot_manager.services.auth_service import AuthService, CurrentUserDp
 from bot_manager.services.user_service import UserService
-from bot_manager.services.email_service import EmailService
 
 
 AuthServiceDp = Annotated[AuthService, Depends(AuthService)]
