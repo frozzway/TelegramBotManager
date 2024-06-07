ce8f235cff7732061edf1477d24a063fdb1e310f
Александр Окунев <a.okunev@mininform74.ru>
2024-06-07 08:39:54 +0500

    Добавил валидатор для преобразования tables.Role в Role (enum)

--- bot_manager/models/auth.py
+++ bot_manager/models/auth.py
@@ -1,6 +1,8 @@
-from pydantic import BaseModel, EmailStr
+from typing import Any, Iterable
+from pydantic import BaseModel, EmailStr, field_validator
 
 from bot_manager.roles import Role
+from bot_manager import tables
 
 
 __all__ = ['UserBase', 'User', 'UserJWT', 'Token', 'Login']
@@ -16,6 +18,18 @@ class UserBase(BaseModel):
 
 class User(UserBase):
     id: int
+
+    # noinspection PyNestedDecorators
+    @field_validator('roles', mode='before')
+    @classmethod
+    def modify_role_types(cls, raw_value: Any):
+        result = []
+        if isinstance(raw_value, Iterable):
+            for role_entity in raw_value:
+                item = Role(role_entity.name) if isinstance(role_entity, tables.Role) else role_entity
+                result.append(item)
+            return result
+        return raw_value
 
 
 class UserJWT(BaseModel):
